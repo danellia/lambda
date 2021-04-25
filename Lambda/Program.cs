cec8d0d4fc2f5e5fd1c62bca0f75c66fbf62f577
danellia <deshkadary2@gmail.com>
2021-04-25 22:38:29 +0700

    ???

---
+++ Lambda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Lambda
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterExpression number = Expression.Parameter(typeof(double), "num");
+            ConstantExpression one = Expression.Constant((double)1, typeof(double));
+            BinaryExpression add = Expression.Add(number, one);
+            Expression<Func<double, double>> lambda1 = Expression.Lambda<Func<double, double>>(add, new ParameterExpression[] { number });
+            Func<double, double> func = lambda1.Compile();
+
+            Console.ReadKey();
+        }
+    }
+}
